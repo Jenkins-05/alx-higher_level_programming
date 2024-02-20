b462eedfd1b2c2785b559b5d70800bc541994a52
Jenkins-05 <kennedynjobvu5@gmail.com>
2024-02-20 04:09:22 -0800

    updated

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -22,3 +22,11 @@ class Student:
 
         If attrs is a list of strings, represents only those attributes
         included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
